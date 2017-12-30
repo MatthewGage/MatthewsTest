3f69c96f56dc86804a37d18560a13af39f4450d6
MatthewGage <34926326+MatthewGage@users.noreply.github.com>
2017-12-30 09:53:53 -0500

    this will be my sandbox.
    its a sandbox of code

---
+++ Untitled-1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Index</title>
+<link href="StyleSheet.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<!-- Header -->
+	<h2 class="header"><strong>Matthew's Sandbox</strong></h2>
+	<!-- About -->
+<p3>Hello, <span class="important">This is a sandbox</span> were you can gather coding information</p3>
+	<p>This sandbox will include the following code</p>
+<li class="feat">Basic/Advanced Html</li>
+<li class="feat">Basic/Advanced CSS</li>
+<li class="feat">Basic PHP</li>
+	<!-- IMG -->
+	<img src="sandbox-illustration-3-dudes_orig.png">
+	
+</body>
+</html>
